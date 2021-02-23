65c21d9c40aa8bd2e8e400785abc2a02feef97ce
Marcos Marx <marcosmarxm@gmail.com>
2021-02-22 22:07:31 -0300

    change operator to plugin folder

--- plugin/operator.py
+++ plugin/operator.py
@@ -1,3 +1,5 @@
+import requests
+from time import sleep
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -14,18 +16,33 @@ class AirbyteTriggerSyncOperator(BaseOperator):
         self.airbyte_conn_id = airbyte_conn_id
         self.source = source
         self.destination = destination
-
-        self._airbyte_api = 'http://airbyte:8000'
+        self._airbyte_api = 'http://172.21.0.1:8001/api/v1'
         self._connection_id = '15bc3800-82e4-48c3-a32d-620661273f28'
 
     def execute(self, context):
-        job = self._sync_connection()
-        job_id = job.get('header').get('')
-        if job.status_code == 200:
-            print("YEAH!")
+        self.job_id = self._sync_connection()
+        self.job_status = self._get_job_status()
 
+        while self.job_status == 'running' or self.job_status == 'pending':
+            sleep(1)
+            self.job_status = self._get_job_status()
+        
+        if self.job_status == 'succeeded':
+            print("Finished sync")
+        else:
+            raise("Something weird happened!")
+
+    def _get_json_response(self, path, payload):
+        r = requests.post(f'{self._airbyte_api}{path}', json=payload)
+        if r.status_code == 200:
+            return r.json()
 
     def _sync_connection(self):
+        path = '/connections/sync'
         payload = {"connectionId": self._connection_id}
-        r = request.post(f'{self.airbyte_api}/v1/connections/sync', json=payload)
-        return r
+        return _get_json_response(path, payload).get('job').get('id')
+
+    def _get_job_status(self):
+        path = '/jobs/get'
+        payload = {"id": self.job_id}
+        return _get_json_response(path, payload).get('job').get('status')
